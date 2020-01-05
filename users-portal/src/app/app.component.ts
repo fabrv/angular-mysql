cc714514c9a6ba20f1b8aaa61e85dfaf89e317cb
fabrv <frivera@nevicad.com>
2020-01-05 02:06:18 -0600

    basic form

--- users-portal/src/app/app.component.ts
+++ users-portal/src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'users-portal';
+  title: string = 'Usuarios';
+  civilStates: Array<string> = ['Soltero', 'Casado', 'Divorciado']
+  mask: Array<any> = [/[1-9]/, /\d/, /\d/, /\d/, ' ' , /\d/, /\d/, /\d/, /\d/, /\d/, ' ' , /\d/, /\d/, /\d/, /\d/]
+  civilState: string = 'Soltero'
+
+  data: { name: string, lastnames: string, civilState: string, cui: string } = {
+    name: '',
+    lastnames: '',
+    civilState: 'Soltero',
+    cui: ''
+  }
 }
--- users-portal/src/app/app.module.ts
+++ users-portal/src/app/app.module.ts
@@ -4,13 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { 
+  MatToolbarModule, 
+  MatInputModule, 
+  MatCardModule, 
+  MatSelectModule,
+  MatButtonModule } from '@angular/material';
+
+import { TextMaskModule } from 'angular2-text-mask'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatCardModule,
+    MatSelectModule,
+    TextMaskModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
