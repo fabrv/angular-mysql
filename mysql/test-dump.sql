1eb18426799678679ebc8c359167d3acf2f82558
fabrv <frivera@nevicad.com>
2020-01-04 22:18:23 -0600

    real data

--- mysql/test-dump.sql
+++ mysql/test-dump.sql
@@ -26,7 +26,7 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 LOCK TABLES `users` WRITE;
-INSERT INTO `users` VALUES (null, '{"data": "data"}');
+INSERT INTO `users` VALUES (null, '{"CUI": "3035727091105", "name": "Juan", "lastname": "Ramirez", "civilState": "soltero"}');
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
